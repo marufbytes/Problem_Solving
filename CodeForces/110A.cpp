6d983b44233c66c759a12b88e85373b26be3eea3
marufbytes <marufahammed215@gmail.com>
2026-05-27 12:27:59 +0600

    counting 4 and 7

---
+++ CodeForces/110A.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string num;
+    cin >> num;
+
+    int count = 0;
+
+    for (int i = 0; i < num.length(); i++)
+    {
+        if (num[i] == '4' || num[i] == '7')
+        {
+            count++;
+        }
+    }
+    
+    if (count == 4 || count == 7)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
+
+    return 0;
+}
